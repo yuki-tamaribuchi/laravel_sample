1bc7ef0e84ebaf53a3cef13cd925ce17b3d6894b
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-05-16 07:59:31 +0900

    Add route

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 
-Route::get('hello', 'App\Http\Controllers\HelloController@getHello');
+Route::get('hello', 'App\Http\Controllers\HelloController@getHello')->name('hello');
+
+
+Route::match(array('GET', 'POST') ,'/accounts/signup', 'App\Http\Controllers\AccountsController@signup')->name('signup');
+Route::match(array('GET', 'POST'), '/accounts/login', 'App\Http\Controllers\AccountsController@login')->name('login');
+Route::get('/accounts/logout', 'App\Http\Controllers\AccountsController@logout')->name('logout');
+Route::get('/accounts/detail/{id}', 'App\Http\Controllers\AccountsController@detail')->name('account_detail');
+Route::match(array('GET', 'POST'), '/accounts/update/{id}', 'App\Http\Controllers\AccountsController@update')->name('account_update');
+Route::match(array('GET', 'POST'), '/accounts/delete/{id}', 'App\Http\Controllers\AccountsController@delete')->name('account_delete');
