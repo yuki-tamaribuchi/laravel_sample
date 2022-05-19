4dba11cd03f552a1c02bd54e65702e20d5a60104
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-05-20 08:10:21 +0900

    Create post store function

--- src/app/Http/Controllers/PostsController.php
+++ src/app/Http/Controllers/PostsController.php
@@ -12,4 +12,23 @@ class PostsController extends Controller
     public function create(){
         return view('posts.create');
     }
+
+    public function store(Request $request){
+        $validator = $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $title = $request->get('title');
+        $content = $request->get('content');
+        $user_id = Auth::user()->id;
+
+        $post = Posts::create([
+            'title' => $title,
+            'content' => $content,
+            'user_id' => $user_id
+        ]);
+
+        return redirect(route('posts.show', ["post" => $post->id]));
+    }
 }
