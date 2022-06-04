26d8f06b0859614a556894160153ebc84f69da7d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-05 07:47:49 +0900

    Modify form

--- src/resources/views/posts/edit.blade.php
+++ src/resources/views/posts/edit.blade.php
@@ -1,17 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>投稿編集</h2>
-<form action="{{ route('posts.update', ['post' => $post->id]) }}" method="PUT">
-	{{ csrf_field() }}
-	タイトル: <input name="title" value="{{ $post->title }}">
-	@if ($errors->first('title'))
-		{{$errors->first('title')}}
-	@endif
-	<br>
-	内容: <input name="content" type="text" value="{{ $post->content }}">
-	<br>
-	<button type="submit">更新</button>
-</form>
+@include('posts.form')
 
 @endsection
