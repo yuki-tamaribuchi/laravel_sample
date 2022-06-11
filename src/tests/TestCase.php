7ece8a78a0ff35031077d43363c58d3a422994c2
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-12 07:45:01 +0900

    Add user and categories create function

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -4,7 +4,23 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+use App\Models\User;
+use App\Models\Categories;
+
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    function create_user(){
+        return User::factory()->create();
+    }
+
+    function create_categories_from_seeder(){
+        $this->seed('CategoriesTableSeeder');
+        return Categories::all();
+    }
+
+    function create_category($category_name){
+        return Categories::create(['name'=>$category_name]);
+    }
 }
