6e5a6599d4c4fdb3393737c55416bc5952e279ae
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-15 00:11:58 +0900

    Create PostsApiController

---
+++ src/app/Http/Controllers/PostsApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\DB;
+
+class PostsApiController extends Controller
+{
+}
