76e1ebb2943db417cc096c60c88f147f8c9b8650
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-16 00:01:41 +0900

    Create store function

--- src/app/Http/Controllers/PostsApiController.php
+++ src/app/Http/Controllers/PostsApiController.php
@@ -32,4 +32,35 @@ class PostsApiController extends Controller
         $post = json_encode($post);
         return response()->json(['post'=>$post]);
     }
+
+    public function store(Request $request){
+        $request_content = $request->getContent();
+        $request_content = json_decode($request_content, true);
+        if(($validation_result=$this->validation($request_content))!=null){
+            return $validation_result;
+        }
+        $title = $request_content['title'];
+        $content = $request_content['content'];
+        $user_id = $request_content['user_id'];
+        $categories = $request_content['categories'];
+        
+        try{
+            $this->posts_table->insert([
+                [
+                    'title'=>$title,
+                    'content'=>$content,
+                    'user_id'=>$user_id,
+                    'created_at'=>date('Y-m-d H:i:s'),
+                    'updated_at'=>date('Y-m-d H:i:s')
+                ]
+            ]);
+            $post = $this->posts_table->where('title', $title)->where('content', $content)->where('user_id', $user_id)->orderBy('id', 'desc')->first();
+            $categories_data = $this->create_categories_data($categories, $post->id);
+            $categories = $this->post_category_table->insert($categories_data);
+        } catch(Exception $e) {
+            $this->posts_table->where('id', $post->id)->delete();
+        }
+        $post = json_encode($post);
+        return response()->json(['post'=>$post], Response::HTTP_CREATED);
+    }
 }
